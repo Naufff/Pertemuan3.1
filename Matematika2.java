035c9362b4285fcb60a95cd7de3d7223ba11ea68
Naufff <114640395+Naufff@users.noreply.github.com>
2023-10-25 22:34:44 +0700

    Add files via upload

---
+++ Matematika2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan3;
+
+import pertemuan2.Matematika;
+
+/**
+ *
+ * @author naufa
+ */
+public class Matematika2 extends Matematika{
+     public int modulus(int a, int b){
+        return a % b;
+     }
+}
